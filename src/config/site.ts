798b3d7a8ebd35e090c8ee3850e330279794b6a9
GabrielAlcantara00 <lokodaskebrada@gmail.com>
2026-09-12 23:27:04 -0300

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -7,13 +7,13 @@ export const siteConfig = {
     starter: {
       name: "STARTER",
       price: "12,90",
-      checkoutUrl: "#planos",
+      checkoutUrl: "https://ggcheckout.app/checkout/v5/wdcLDT15r5lPiC0Nz2U1",
     },
     pro: {
       name: "PRO",
       price: "29,90",
       installments: "2x de R$ 14,95",
-      checkoutUrl: "#planos",
+      checkoutUrl: "https://ggcheckout.app/checkout/v5/mE5or2yp85g1cvgOYLJD",
     },
   },
 };
